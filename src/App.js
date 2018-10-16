5d20d566ef287606fe9b0c7464f9f9289a5af35a
Michael Robards <mikerobards@icloud.com>
2018-10-16 07:03:19 -0400

    add foursquare api to app.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import Map from './components/Map';
+import foursquareAPI from './API';
+
 
 class App extends Component {
+  componentDidMount(){
+    foursquareAPI.search({
+      near: "Atlanta, GA",
+      query: "pizza",
+      limit: 10
+    }).then(results => console.log(results));
+  }
   render() {
     return (
       <div className="App">
