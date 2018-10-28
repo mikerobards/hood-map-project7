cbb15de9673815b1c5125b5b5bb41c9e269741fb
Michael Robards <mikerobards@icloud.com>
2018-10-28 10:03:56 -0400

    add click to list item

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 
 export default class ListItem extends Component {
   render() {
-    return (<li className="list-group-item">
+    return (<li className="list-group-item"
+    onClick={() => this.props.listItemClick(this.props)}>
       {this.props.name}
     </li>);
   }
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,6 +2,38 @@ import React, { Component } from 'react';
 import VenueList from './VenueList';
 
 export default class Sidebar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      query: '',
+      venues: []
+    };
+  }
+
+  workFilterVenues = () => {
+    if(this.state.query.trim() !== "") {
+      const venues = this.props.venues.filter(venue =>
+        venue.name.toLowerCase().includes(this.state.query.toLowerCase()))
+        return venues;
+    }
+      return this.props.venues;
+  };
+
+  workChange = event => {
+      this.setState({ query: event.target.value });
+
+      const markers = this.props.venues.map(venue => {
+        const matched = venue.name.toLowerCase().includes(event.target.value.toLowerCase());
+        const marker = this.props.markers.find(marker => marker.id === venue.id);
+        if(matched) {
+          marker.isVisible = true;
+        } else {
+          marker.isVisible = false;
+        }
+        return marker;
+      });
+      this.props.updateSuperState({ markers })
+  };
 
   render() {
     return (<div>
