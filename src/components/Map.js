0c49793e77d1e3e56ef981bfbe54ed04bbc5d02a
Michael Robards <mikerobards@icloud.com>
2018-10-28 09:48:21 -0400

    add info window

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from 'react-google-maps';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker, InfoWindow } from 'react-google-maps';
 
 const MyMapComponent = withScriptjs(withGoogleMap(props => (
   <GoogleMap
@@ -11,7 +11,13 @@ const MyMapComponent = withScriptjs(withGoogleMap(props => (
     {props.markers && props.markers
       .filter(marker => marker.isVisible)
       .map((marker, index) => (
-        <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} />
+        <Marker key={index} position={{ lat: marker.lat, lng: marker.lng }} onClick={() => props.markerClick(marker)}>
+          {marker.isOpen && (
+            <InfoWindow>
+            <p>Hello</p>
+            </InfoWindow>
+          )}
+        </Marker>
       ))}
   </GoogleMap>
 ))
